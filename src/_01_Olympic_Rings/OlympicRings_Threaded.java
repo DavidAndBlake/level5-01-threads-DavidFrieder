b921099eea4261e8afab3103451721c07168eab4
davidandblake <davidfrieder@gmail.com>
2021-11-28 18:05:28 -0800

    Finished Olympic rings and robot race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,40 @@
 package _01_Olympic_Rings;
 
 
-public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+import org.jointheleague.graphical.robot.Robot;
+
+public class OlympicRings_Threaded
+{
+    // Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+
+    public static void main(String[] args)
+    {
+        Robot[] robots = new Robot[5];
+        for (int i = 0; i < 5; i++)
+        {
+         robots[i] = new Robot();
+         robots[i].hide();
+        }
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.length; i++)
+        {
+            int x = i;
+            robots[i].show();
+            threads[i] = new Thread(() ->
+            {
+                robots[x].setX(200 + x * 200);
+                robots[x].penDown();
+                System.out.println(x);
+                for (int j = 0; j < 360; j++)
+                {
+                    robots[x].move(1);
+                    robots[x].turn(1);
+                }
+            }
+            );
+            threads[i].start();
+        }
+    }
 
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,44 @@
 package _02_Advanced_Robot_Race;
 
-public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
-	// This time, use threads to make all of the robots go at the same time.
+
+import org.jointheleague.graphical.robot.Robot;
+
+import java.util.Random;
+
+public class AdvancedRobotRace
+{
+    static
+    // Re-do the robot race recipe from level 3 module 0.
+    // This time, use threads to make all of the robots go at the same time.
+    Thread[] threads = new Thread[5];
+
+    public static void main(String[] args)
+    {
+        Robot[] robots = new Robot[5]; //New robot array called robots
+
+        Random random = new Random();
+        for (int k = 0; k < 5; k++)
+        {
+            robots[k] = new Robot();
+            int x = k;
+            threads[x] = new Thread(() ->
+            {
+                robots[x].setX(100 + x * 200);
+                robots[x].setY(600);
+                    while (robots[x].getY() > 50)
+                    {
+                        robots[x].setSpeed(random.nextInt(9)+1);
+                        robots[x].move(30);
+                    }
+
+
+            });
+            int places = 1;
+            if(robots[x].getY() <= 50){
+            System.out.println("Robot # " + x + " ranks at #" + places);
+            places++;
+        }
+            threads[x].start();
+        }
+    }
 }
